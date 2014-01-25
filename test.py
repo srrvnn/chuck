5a897ca5c3826b88aa3f888deb17c3998eb58e2c
Anirudh Ranganath <anipub@hotmail.com>
2014-01-25 14:06:25 -0800

    I thought I added server :\

--- test.py
+++ test.py
@@ -1,9 +1,21 @@
 from pinterest.client import raw_client
+from pinterest.client import ApiClient
 
+USER_SECRET = "MTQzNTgwODo0OTUxODUwNTg5MzY2Njg0NzM6MnwxMzkwNjY5MzAzOjAtLTIwMTEzMGVhMzYyYzM1NTEyZTA1NjJlNGZiYTJjMTBiN2RhMmEwZjU="
 APP_SECRET = "5e0927fa47bf3e3eab3d171b4db596f9684fa75d"
 APP_ID = "1435808"
+
+'''
 my_client = raw_client(APP_ID, APP_SECRET)
 
-response = my_client.users.theanirudh.get()
+#response = my_client.users.icecreamcohen.pins.get()
+'''
 
-print response
+apic = ApiClient(APP_ID, APP_SECRET)
+apic.authorize(USER_SECRET)
+
+
+
+
+
+#print response
