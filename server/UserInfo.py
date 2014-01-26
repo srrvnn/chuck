3c5abafea8c18632bfca82db3447c8df032ebc13
Anirudh Ranganath <anipub@hotmail.com>
2014-01-25 21:38:22 -0800

    small bug fix

--- server/UserInfo.py
+++ server/UserInfo.py
@@ -6,23 +6,23 @@ APP_SECRET = "5e0927fa47bf3e3eab3d171b4db596f9684fa75d"
 APP_ID = "1435808"
 
 def getUserRetailPins(USER_SECRET,user_id):\
-  userpins = getUserPins(USER_SECRET,user_id)
-  client = pymongo.MongoClient('107.170.247.246', 27017)
-  records = client['chuck']['retailers'].find()
-  retail_pins = {}
-  for record in records:
-    items = record['items']
-    for item in items:
-      retail_pins[item['pinid']] = 1
-  ret = []
-  for pin in userpins:
-    if 'origin_pin' in pin.keys() and pin['origin_pin'] in retail_pins.keys():
-      ret.append(pin)
-  return ret
+    userpins = getUserPins(USER_SECRET,user_id)
+    client = pymongo.MongoClient('107.170.247.246', 27017)
+    records = client['chuck']['retailers'].find()
+    retail_pins = {}
+    for record in records:
+	items = record['items']
+	for item in items:
+	    retail_pins[item['pinid']] = 1
+    ret = []
+    for pin in userpins:
+	if 'origin_pin' in pin.keys() and pin['origin_pin'] in retail_pins.keys():
+	    ret.append(pin)
+    return ret
   
 def getUserPins(USER_SECRET,user_id):
-  my_client = raw_client(APP_ID, APP_SECRET)
-  my_client.authorize(USER_SECRET)
-  pins = my_client.users(user_id).pins.get()
-  return pins[0]
+    my_client = raw_client(APP_ID, APP_SECRET)
+    my_client.authorize(USER_SECRET)
+    pins = my_client.users(user_id).pins.get()
+    return pins[0]
 
